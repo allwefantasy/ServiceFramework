47f7250cd4f6dbb7674c3ba0631f453501bcef50
WilliamZhu <allwefantasy@gmail.com>
2017-09-16 10:44:16 +0800

    allow no httpclient depencecy

--- serviceframework-web/src/main/java/net/csdn/modules/transport/TransportModule.java
+++ serviceframework-web/src/main/java/net/csdn/modules/transport/TransportModule.java
@@ -1,4 +1,5 @@
-package net.csdn.modules.transport;/**
+package net.csdn.modules.transport;
+/**
  * BlogInfo: WilliamZhu
  * Date: 12-5-29
  * Time: 下午5:09
@@ -6,11 +7,20 @@ package net.csdn.modules.transport;/**
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Singleton;
+import net.csdn.bootstrap.loader.impl.ServiceLoader;
+import net.csdn.common.logging.CSLogger;
+import net.csdn.common.logging.Loggers;
 
 
 public class TransportModule extends AbstractModule {
+    private CSLogger logger = Loggers.getLogger(TransportModule.class);
+
     @Override
     protected void configure() {
-        bind(HttpTransportService.class).to(DefaultHttpTransportService.class).in(Singleton.class);
+        try {
+            bind(HttpTransportService.class).to(DefaultHttpTransportService.class).in(Singleton.class);
+        } catch (Exception e) {
+            logger.warn("HttpTransportService bind exception", e.getCause());
+        }
     }
 }
