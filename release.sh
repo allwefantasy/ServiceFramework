928a48676f7627e0a49af07a0660050cc5dffbcf
WilliamZhu <allwefantasy@gmail.com>
2018-12-22 14:56:07 +0800

    upgrade to 2.0.0 relase

--- release.sh
+++ release.sh
@@ -1,18 +1,3 @@
-rm -rf release/dependency/
-mkdir -p release/dependency/
-mvn clean
-mvn compile
-mvn dependency:copy-dependencies
-cp -rf target/dependency/* release/dependency/
-
-git rm -rf  release/classes
-cp -rf target/classes release
-
-# auto push to git
-#git add .
-#log="auto-deploy"
-#if [ "$1" != "" ];then
-#    log=$1
-#fi
-#git commit -m'auto-deploy'
-#git push origin master
+mvn clean deploy -DskipTests -Pjetty-9 -Pweb-include-jetty-9 -P release-sign-artifacts
+#https://oss.sonatype.org/#stagingRepositories
+#mvn versions:set -DnewVersion={version}
