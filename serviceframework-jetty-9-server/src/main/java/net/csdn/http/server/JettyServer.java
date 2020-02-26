a7cd63b8cc52438b6c7dea21cf3e7166a8153ee9
WilliamZhu <allwefantasy@gmail.com>
2020-02-26 14:46:07 +0800

    update

--- serviceframework-jetty-9-server/src/main/java/net/csdn/http/server/JettyServer.java
+++ serviceframework-jetty-9-server/src/main/java/net/csdn/http/server/JettyServer.java
@@ -34,7 +34,7 @@ public class JettyServer {
                                    boolean sessionEanble,
                                    AbstractHandler abstractHandler) {
 
-        HandlerList handlers = new HandlerList(true);
+        HandlerList handlers = new HandlerList();
 
         if (staticEnable) {
             ResourceHandler resource_handler = new ResourceHandler();
