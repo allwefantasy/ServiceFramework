8acb34e25527048144fa94734a6db463913849cc
jiachuan.zhu <chncaesar@gmail.com>
2022-09-16 17:48:25 +0800

    Merge log4j 2 configurations

--- serviceframework-common/src/main/java/net/csdn/common/logging/log4j/LogConfigurator.java
+++ serviceframework-common/src/main/java/net/csdn/common/logging/log4j/LogConfigurator.java
@@ -28,29 +28,29 @@ public class LogConfigurator {
 
     private static ImmutableMap<String, String> replacements = new MapBuilder<String, String>()
             .put("console", "org.apache.logging.log4j.core.appender.WriterAppender")
-            .put("async", "org.apache.log4j.AsyncAppender")
-            .put("dailyRollingFile", "org.apache.log4j.DailyRollingFileAppender")
-            .put("externallyRolledFile", "org.apache.log4j.ExternallyRolledFileAppender")
-            .put("file", "org.apache.log4j.FileAppender")
-            .put("hadoo", "org.apache.log4j.FileAppender")
-            .put("jdbc", "org.apache.log4j.jdbc.JDBCAppender")
-            .put("jms", "org.apache.log4j.net.JMSAppender")
-            .put("lf5", "org.apache.log4j.lf5.LF5Appender")
-            .put("ntevent", "org.apache.log4j.nt.NTEventLogAppender")
-            .put("null", "org.apache.log4j.NullAppender")
-            .put("rollingFile", "org.apache.log4j.RollingFileAppender")
-            .put("smtp", "org.apache.log4j.net.SMTPAppender")
-            .put("socket", "org.apache.log4j.net.SocketAppender")
-            .put("socketHub", "org.apache.log4j.net.SocketHubAppender")
-            .put("syslog", "org.apache.log4j.net.SyslogAppender")
-            .put("telnet", "org.apache.log4j.net.TelnetAppender")
+            .put("async", "org.apache.logging.log4j.core.appender.AsyncAppender")
+            .put("dailyRollingFile", "org.apache.logging.log4j.core.appender.RollingFileAppender")
+//            .put("externallyRolledFile", "org.apache.log4j.ExternallyRolledFileAppender")
+            .put("file", "org.apache.logging.log4j.core.appender.FileAppender")
+            .put("hadoo", "org.apache.logging.log4j.core.appender.FileAppender")
+            .put("jdbc", "org.apache.logging.log4j.core.appender.db.jdbc.JDBCAppender")
+            .put("jms", "org.apache.logging.log4j.core.appender.mom.JMSAppender")
+//            .put("lf5", "org.apache.log4j.lf5.LF5Appender")
+//            .put("ntevent", "org.apache.log4j.nt.NTEventLogAppender")
+            .put("null", "org.apache.logging.log4j.core.appender.NullAppender")
+            .put("rollingFile", "org.apache.logging.log4j.core.appender.RollingFileAppender")
+            .put("smtp", "org.apache.logging.log4j.core.appender.SMTPAppender")
+            .put("socket", "org.apache.logging.log4j.core.appender.SocketAppender")
+            .put("socketHub", "org.apache.logging.log4j.core.appender.SocketHubAppender")
+            .put("syslog", "org.apache.logging.log4j.core.appender.SyslogAppender")
+//            .put("telnet", "org.apache.log4j.net.TelnetAppender")
                     // layouts
-            .put("simple", "org.apache.log4j.SimpleLayout")
-            .put("html", "org.apache.log4j.HTMLLayout")
-            .put("pattern", "org.apache.log4j.PatternLayout")
+            .put("simple", "org.apache.logging.log4j.core.layout.SyslogLayout")
+            .put("html", "org.apache.logging.log4j.core.layout.HtmlLayout")
+            .put("pattern", "org.apache.logging.log4j.core.layout.PatternLayout")
             .put("consolePattern", "org.apache.logging.log4j.core.layout.PatternLayout")
-            .put("ttcc", "org.apache.log4j.TTCCLayout")
-            .put("xml", "org.apache.log4j.XMLLayout")
+//            .put("ttcc", "org.apache.log4j.TTCCLayout")
+            .put("xml", "org.apache.logging.log4j.core.layout.XmlLayout")
             .immutableMap();
 
     public static void configure(Settings settings) {
@@ -66,7 +66,7 @@ public class LogConfigurator {
 
         Properties props = new Properties();
         for (Map.Entry<String, String> entry : settingsBuilder.build().getAsMap().entrySet()) {
-            String key = "log4j." + entry.getKey();
+            String key = entry.getKey();
             String value = entry.getValue();
 
             if (replacements.containsKey(value)) {
@@ -81,7 +81,12 @@ public class LogConfigurator {
                 props.setProperty(key, value);
             }
         }
-        LoggerContext context = (LoggerContext) LogManager.getContext(false);
+        LoggerContext context = (LoggerContext) LogManager.getContext(true);
+        Configuration oldContext = context.getConfiguration();
+
+        for (Map.Entry<String, String> prop : oldContext.getProperties().entrySet()) {
+            props.put(prop.getKey(), prop.getValue());
+        }
         Configuration config = new PropertiesConfigurationBuilder()
                 .setConfigurationSource(ConfigurationSource.NULL_SOURCE)
                 .setRootProperties(props)
